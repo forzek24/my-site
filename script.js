50761d04239728c085ca9f293b0a388784903fa1
forZeq <matiem.ah7@gmail.com>
2026-09-19 21:56:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let count = 0;
+let count = localStorage.getItem('clicks') ? parseInt(localStorage.getItem('clicks')) : 0;
+
 const btn = document.getElementById('btn');
 const counter = document.getElementById('counter');
+
+counter.textContent = `Нажатий: ${count}`;
 
 btn.addEventListener('click', () => {
     count++;
     counter.textContent = `Нажатий: ${count}`;
+    localStorage.setItem('clicks', count);
 });
